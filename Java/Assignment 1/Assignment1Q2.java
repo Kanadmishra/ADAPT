a03a6e5858bdf65b433107b4f3b7d1505924626c
Kanadmishra <kanad.mishra@gmail.com>
2021-02-12 16:49:09 +0530

    This is assignment1 of java

---
+++ Java/Assignment 1/Assignment1Q2.java
@@ -0,0 +1,39 @@
+package prac;
+
+public class Assignment1Q2 {
+
+	public static void main(String[] args) {
+
+		AmstrongNumberInRange l = new AmstrongNumberInRange();
+		l.AmstrongNumber();
+		
+
+	}
+
+}
+class AmstrongNumberInRange {
+	public void AmstrongNumber ()
+	{
+		for (int i =100 ; i<999 ; i++)
+		{
+			int n = i;int k;
+			int total = 0;
+			while(n!=0)
+			{
+				k=n%10;
+				total=total+(k*k*k);
+				n=n/10;
+			}
+			if(total==i)
+			{
+				System.out.println(i);
+			}
+			else
+			{
+				continue;
+			}
+			
+		}
+	}
+}
+
